f2a5b99a11e985eb787395215faa5cb7443cdd89
Abdelfatah Ashour <abdelfatahashour4@gmail.com>
2022-02-25 16:13:57 +0200

    Display images like a 360 Degree

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Pannellum } from "pannellum-react";
+import myImage1 from "./images/IMG_20220221_122313_00_294.jpg";
+import myImage2 from "./images/IMG_20220221_122830_00_300.jpg";
+import myImage3 from "./images/IMG_20220221_123040_00_305.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Flowtechtics</h1>
+      <Pannellum
+        width="100%"
+        height="500px"
+        image={myImage2}
+        pitch={10}
+        yaw={180}
+        hfov={110}
+        autoLoad
+        showControls={false}
+        onLoad={() => {
+          console.log("panorama loaded");
+        }}
+      ></Pannellum>
+    </div>
+  );
+}
+
+export default App;
